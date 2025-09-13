c917cc769fdc965c14d901b63be8f05af01ce451
Chanodyapraveen <135603936+Chanodyapraveen@users.noreply.github.com>
2025-09-14 01:26:06 +0530

    Add initial HTML template for the online course platform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TeamSection from './components/teamsection';
 
 function App() {
   return (
     <div className="App">
       <h1>Online Course Platform</h1>
+      <TeamSection />
     </div>
   );
 }
